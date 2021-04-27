62719adc7e5e133db3c697a5f92819e988317ee0
adrian-swifter <milosffh@yahoo.com>
2021-04-27 04:39:36 +0200

    gitignore fix

--- src/firebase/firebase_storage.js
+++ src/firebase/firebase_storage.js
@@ -2,6 +2,14 @@ import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: "AIzaSyC52MLbphe9Iq5b7B7eP33W4KWhz3qEDqY",
+  authDomain: "reel-maker-7ed1f.firebaseapp.com",
+  databaseURL: "https://reel-maker-7ed1f.firebaseio.com/",
+  projectId: "reel-maker-7ed1f",
+  storageBucket: "reel-maker-7ed1f.appspot.com",
+  messagingSenderId: "368974428901",
+  appId: "1:368974428901:web:e6243a4e10b85c4907320c",
+};
 // Initialize Firebase
 export const app = firebase.initializeApp(firebaseConfig);
