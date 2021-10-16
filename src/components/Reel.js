898f21a2c43431bcc51370a916e46cf595862575
adrian-swifter <milosffh@yahoo.com>
2021-10-16 14:19:52 +0200

    finally fixed selected track nitial

--- src/components/Reel.js
+++ src/components/Reel.js
@@ -4,20 +4,12 @@ import Waveform from "../components/waveform/Waveform";
 import PlayList from "../components/waveform/PlayList";
 import useFirestore from "../hooks/useFirestore";
 
-const dummyTracks = [
-  {
-    id: 0,
-    title: "Brahms: St Anthony Chorale - Theme, Two Pianos Op.56b",
-    url: "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3",
-  },
-];
-
 function Reel() {
   const location = useLocation();
   const allSongs = useFirestore("songs");
   const allReels = useFirestore("reels");
-  const [selectedTrack, setSelectedTrack] = useState(dummyTracks[0]);
-  //console.log(allSongs, allReels.songs[2].hash, location.hash);
+
+  const [selectedTrack, setSelectedTrack] = useState({ url: "initial value" });
   const [filteredSongs, setFilteredSongs] = useState([]);
   const [hash, setHash] = useState(0);
 
@@ -35,12 +27,11 @@ function Reel() {
       if (allReels.songs[hash][0].includes(song.id)) {
         tempARr.push(song);
       }
+      setSelectedTrack(tempARr[0]);
       setFilteredSongs(tempARr);
     });
   }, [allReels.songs, hash]);
 
-  console.log(hash, "hash");
-  console.log(filteredSongs, "filtered");
   return (
     <div className="reel">
       <Waveform url={selectedTrack.url} />
